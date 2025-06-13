054a4c59829924562eb0f5144e544fb50aa0f3ba
Giovanni-Hernandez10 <giovannihernandez10209@gmail.com>
2025-06-13 15:45:58 -0700

    initial project setup and supabase authentication

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+import {Routes, Route, BrowserRouter} from "react-router-dom"
+import {Layout} from "./layout/Layout.tsx"
+import {AuthenticationPage} from "./auth/AuthenticationPage.tsx";
+import {HomePage} from "./layout/HomePage.tsx";
+
+function App() {
+  return (
+      <BrowserRouter>
+          <Routes>
+            <Route path={"/sign-in/*"} element={<AuthenticationPage/>}/>
+            <Route path={"/sign-up"} element={<AuthenticationPage/>}/>
+            <Route element={<Layout/>}>
+                <Route path={"/"} element={<HomePage/>}/>
+            </Route>
+        </Routes>
+      </BrowserRouter>
+  )
+}
+
+export default App
