0988cc6e27c6231662f4503fb261dc022911b802
Giovanni-Hernandez10 <giovannihernandez10209@gmail.com>
2025-06-14 14:26:35 -0700

    intial authentication page ui without supabase functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Routes, Route, BrowserRouter} from "react-router-dom"
 import {Layout} from "./layout/Layout.tsx"
 import {AuthenticationPage} from "./auth/AuthenticationPage.tsx";
+// import {LoginForm} from "@/auth/login-form-component.tsx";
 import {HomePage} from "./layout/HomePage.tsx";
 
 function App() {
--- frontend/src/auth/AuthenticationPage.tsx
+++ frontend/src/auth/AuthenticationPage.tsx
@@ -1,27 +1,38 @@
 import "react"
-import {useState, useEffect} from "react";
-import {supabase} from "../supabase/supabaseClient.ts";
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import type {Session} from '@supabase/supabase-js';
+// import {useState, useEffect} from "react";
+// import {supabase} from "../supabase/supabaseClient.ts";
+// import { Auth } from '@supabase/auth-ui-react'
+// import { ThemeSupa } from '@supabase/auth-ui-shared'
+// import type {Session} from '@supabase/supabase-js';
+import {LoginForm} from "@/auth/login-form-component.tsx";
+
+// export function AuthenticationPage() {
+//     const [session, setSession] = useState<Session | null>(null);
+//     useEffect(() => {
+//       supabase.auth.getSession().then(({ data: { session } }) => {
+//           setSession(session)
+//       })
+//       const {
+//         data: { subscription },
+//       } = supabase.auth.onAuthStateChange((_event, session) => {
+//           setSession(session)
+//       })
+//       return () => subscription.unsubscribe()
+//     }, [])
+//     if (!session) {
+//         return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+//     }
+//     else {
+//         return (<div>Logged in!</div>)
+//     }
+// }
 
 export function AuthenticationPage() {
-    const [session, setSession] = useState<Session | null>(null);
-    useEffect(() => {
-      supabase.auth.getSession().then(({ data: { session } }) => {
-          setSession(session)
-      })
-      const {
-        data: { subscription },
-      } = supabase.auth.onAuthStateChange((_event, session) => {
-          setSession(session)
-      })
-      return () => subscription.unsubscribe()
-    }, [])
-    if (!session) {
-        return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
-    }
-    else {
-        return (<div>Logged in!</div>)
-    }
+    return (
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm">
+                <LoginForm />
+            </div>
+        </div>
+    )
 }
